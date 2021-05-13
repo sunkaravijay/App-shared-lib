b118047087a1217668c579dab768abe43b7a791e
sunkaravijay <57646123+sunkaravijay@users.noreply.github.com>
2021-05-13 06:15:12 -0400

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,6 +2,8 @@ def test(Map config=[:], Closure body={}){
  echo config.command2
  echo 'Testing..'
  sh './mvnw'+' '+config.command2
-
+ junit '**/target/checkstyle-checker.xml'
+ junit '**/target/checkstyle-result.xml'
+ junit '**/target/checkstyle-suppressions.xml'
 
 }
